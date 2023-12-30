b7ccad7df549a07af366c43368e4c8ee81182f4e
MichaelSchimittDev <michaelschimittpro@gmail.com>
2023-12-30 02:16:58 -0300

    component menu-title finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SmallCardComponent } from "./components/small-card/small-card.component";
+import { BigCardComponent } from "./components/big-card/big-card.component";
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, SmallCardComponent]
+    imports: [CommonModule, RouterOutlet, SmallCardComponent, BigCardComponent, MenuTitleComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
